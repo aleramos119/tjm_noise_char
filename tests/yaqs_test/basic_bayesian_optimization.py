9bd52111e2c5dd1988fd1128b6480a708e9c0043
Alejandro Ramos <aleramos119@gmail.com>
2025-05-06 13:14:49 +0200

    SingleTaskGP with noise

--- tests/yaqs_test/basic_bayesian_optimization.py
+++ tests/yaqs_test/basic_bayesian_optimization.py
@@ -10,7 +10,7 @@ from botorch.fit import fit_gpytorch_mll
 from gpytorch.mlls import ExactMarginalLogLikelihood
 
 
-from botorch.acquisition import LogExpectedImprovement
+from botorch.acquisition import LogExpectedImprovement,UpperConfidenceBound
 
 
 from botorch.optim import optimize_acqf
@@ -51,13 +51,14 @@ def plot_model(gp, xplot, yplot, iter=0):
     plt.xlabel('x')
     plt.ylabel('f(x)')
     plt.savefig(f'plot/gp_model_{iter}.png')
+    plt.close()
 
 
 
 #%% 
 
 n_init = 1
-n_iter = 10
+n_iter = 40
 d=1
 m=1
 bounds_list = [[0,1]]
@@ -65,6 +66,9 @@ bounds_list = [[0,1]]
 num_restarts = 5
 raw_samples = 20
 
+acq_name="LEI"
+beta=0.1
+noise=1e-6
 
 #%%
 
@@ -90,14 +94,16 @@ for i in range(n_init):
 # Plot the model with the standard deviation
 
 for i in range(n_iter):
+
+    train_Yvar = torch.full_like(Y_train, noise)
+
     gp = SingleTaskGP(
         train_X=X_train,
         train_Y=Y_train,
+        train_Yvar=train_Yvar,
         input_transform=Normalize(d=d),
         outcome_transform=Standardize(m=m),
     ).to(X_train)
-
-    gp.likelihood.noise_covar.noise = torch.tensor(0.0, dtype=torch.double)
 
     mll = ExactMarginalLogLikelihood(gp.likelihood, gp)
     fit_gpytorch_mll(mll)
@@ -105,11 +111,16 @@ for i in range(n_iter):
 
     plot_model(gp, xplot, yplot, iter=i)
 
-    logNEI = LogExpectedImprovement(model=gp, best_f=Y_train.min(), maximize=False)
+    if acq_name == "LEI":
+        acqf = LogExpectedImprovement(model=gp, best_f=Y_train.min(), maximize=False)
+    
+    if acq_name == "UCB":
+        acqf = UpperConfidenceBound(model=gp, beta=beta, maximize=False)
+
 
 
     candidate, acq_value = optimize_acqf(
-        logNEI, bounds=bounds, q=1, num_restarts=num_restarts, raw_samples=raw_samples,
+        acqf, bounds=bounds, q=1, num_restarts=num_restarts, raw_samples=raw_samples,
     )
 
 
@@ -121,23 +132,10 @@ for i in range(n_iter):
     loss_history.append(loss)
 
 
-
-#%%
-Y_train
-# %%
-plt.plot(xplot, yplot, label='Loss function')
-plt.plot(X_train.numpy(), Y_train.numpy(), 'o', label='Initial data')
-
-# %%
 plt.plot(np.array(x_history)[:,0], '-', label='history')
 plt.axhline(y=0.5, color='r', linestyle='--', label='y=0.5')
 plt.legend()
 plt.savefig('plot/x_history.png')
-# %%
-with torch.no_grad():
-    posterior = gp.posterior(X_train)
-    print("Standard deviation at training points:", posterior.variance.sqrt().numpy())
-# %%
-print("Noise level:", gp.likelihood.noise_covar.noise.item())
+plt.close()
 
-# %%
+#%%
